a3e4f1c416de9bbef6406c19271862ff94a56b68
Yasmin <yasminlaurentino@edu.unifil.br>
2025-03-21 19:09:29 -0300

    Atualização Tutorial.js

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./tutorial.css"; 
+import RaioIcone from "./public/luxa.org-pixelate-01-raio-png-removebg-preview.png";
 
 function Tutorial() {
     const [message, setMessage] = useState("Testando conexão...");
@@ -10,7 +11,30 @@ function Tutorial() {
         .catch((err) => setMessage("Erro ao conectar ao backend!"));
     }, []);
   
-    return <h1>{message}</h1>;
+        return (
+          <div className="tutorial">
+           <img src={RaioIcone} className="icone-raio" alt="Ícone de Raio" />
+            <span className="flashcards">FLASHCARDS</span>
+            <div className="container">
+              <span className="como">Como jogar:</span>
+              <div className="texto1-tutorial">
+                <li>
+                  O jogo consiste em perguntas e respostas, a cada nível será apresentado um desafio de programação em JAVA,
+                  o seu objetivo é escrever o código correto para passar de fase.
+                </li>
+              </div>
+              <div className="texto2-tutorial">
+                <li>
+                  Lembre-se de que cada vírgula importa, caso algo esteja minimamente fora de ordem, ou faltando um ponto e vírgula,
+                  a sua resposta será considerada errada.
+                </li>
+              </div>
+            </div>
+            <button className="botao">
+              <p className="botao-entendi">Entendi!</p>
+            </button>
+            </div>
+  );
 }
 
 export default Tutorial;
