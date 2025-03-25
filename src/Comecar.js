27e7504f47abe13d623b4e9a5a53577766b9c35a
Bruninha19 <140767534+Bruninha19@users.noreply.github.com>
2025-03-25 19:35:50 -0300

    Update Comecar.js

--- src/Comecar.js
+++ src/Comecar.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./comecar.css";
+import "./nickname.css";
 import logo from "./public/A_pixel_art_of_a_small__cute_dog_wearing_a_red_sup-removebg-preview.png";
 import RaioIcone from "./public/luxa.org-pixelate-01-raio-png-removebg-preview.png";
+import Config from "./public/font-awesome-4.7.0/css/font-awesome.min.css";
 
 function Comecar() {
   const [message, setMessage] = useState("Testando conexão...");
@@ -21,32 +22,39 @@ function Comecar() {
 
   return (
     <>
-      <img src={logo} className="logo-cachorro" alt="Cachorro fofo" />
-      <i className="fas fa-cog config"></i>
-      <div className="container">
-        <div className="cinza_escuro">
-          <div className="comecar-logo">
-            <div className="comecar-logo-text">
-            <img src={RaioIcone} className="comecar-icone-raio" alt="Ícone de Raio" />
-              <div className="comecar-flashcards-wrapper">
-                <div className="comecar-flashcards">FLASHCARDS</div>
-                <div className="comecar-text-shadow1">FLASHCARDS</div>
+      <img 
+        src={logo} className="nickname-logo-cachorro"
+        alt="Cachorro pixelado"
+      />
+      <i src={Config}  id="cog" className="fa fa-cog config"></i>
+      <div className="nickname-container">
+        <div className="nickname-cinza_escuro">
+          <div className="nickname-logo">
+            <div className="nickname-logo-text">
+              <img 
+                src={RaioIcone} 
+                className="nickname-icone-raio" 
+                alt="Ícone de Raio"
+              />
+              <div className="nickname-flashcards-wrapper">
+                <div className="nickname-flashcards">FLASHCARDS</div>
+                <div className="nickname-text-shadow1">FLASHCARDS</div>
               </div>
-              <span className="comecar-java">JAVA</span>
-              <div className="comecar-edition-wrapper">
-                <div className="comecar-edition">edition</div>
-                <div className="comecar-text-shadow2">edition</div>
+              <span className="nickname-java">JAVA</span>
+              <div className="nickname-edition-wrapper">
+                <div className="nickname-edition">edition</div>
+                <div className="nickname-text-shadow2">edition</div>
               </div>
               <div className="nickname">
-                <form className="formulario">
+                <form className="nickname-formulario">
                   <input 
                     type="text" 
                     id="nick" 
                     name="nick" 
                     placeholder="Digite seu nome"
                   />
-                  <button className="tutorial-botao" onClick={iniciar}>
-                    <p className="botao-entendi">Entendi!</p>
+                  <button className="nickname-botaoIniciar">
+                    <p className="nickname-iniciar">Iniciar</p>
                   </button>
                 </form>
               </div>
