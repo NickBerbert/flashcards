535449cab8eb98d5e18a3a6ea50a1f7bcc772ff3
Bruninha19 <140767534+Bruninha19@users.noreply.github.com>
2025-03-25 19:25:37 -0300

    Update Tutorial.js

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -1,47 +1,47 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom"; 
-import "./tela-inicial.css"; 
-import logo from "./public/A_pixel_art_of_a_small__cute_dog_wearing_a_red_sup-removebg-preview.png";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./tutorial.css"; 
 import RaioIcone from "./public/luxa.org-pixelate-01-raio-png-removebg-preview.png";
-import Config from "./public/font-awesome-4.7.0/css/font-awesome.min.css";
 
-function TelaInicial() {
-      useEffect(() => {
-        fetch("http://localhost:5000/tutorial")
-          .then((res) => res.json())
-      }, []);
-  const navigate = useNavigate();
-  return (
-    <div className="container">
-      <div className="telaInicial-cinza_escuro">
-      <div className="telaInicial-logo-container">
-        <img src={logo} className="telaInicial-logo-cachorro" alt="Cachorro fofo" />
-        <div className="telaInicial-logo-text">
-          <div className="telaInicial-flashcards-wrapper">
-            <div className="telaInicial-flashcards">FLASHCARDS</div>
-            <div className="telaInicial-text-shadow1">FLASHCARDS</div>
-            <img src={RaioIcone} className="telaInicial-icone-raio" alt="Ícone de Raio" />
-          </div>
-          <span className="telaInicial-java">JAVA</span>
+function Tutorial() {
+    const [message, setMessage] = useState("Testando conexão...");
+    useEffect(() => {
+      fetch("http://localhost:5000/tutorial")
+        .then((res) => res.json())
+        .then((data) => setMessage(`Backend conectado: ${data.message}`))
+        .catch((err) => setMessage("Erro ao conectar ao backend!"));
+    }, []);
 
-          <div className="telaInicial-edition-wrapper">
-            <div className="telaInicial-edition">edition</div>
-            <div className="telaInicial-text-shadow2">edition</div>
-          </div>
-        </div>
-      </div>
-      
-      <i src={Config} id="cog" className="fa fa-cog"></i>
+    const navigate = useNavigate();
 
-      </div>
-
-      <div className="telaInicial-botoes">
-        <p className="telaInicial-botao" onClick={() => navigate("/comecar")}>Começar</p>
-        <p className="telaInicial-botao" onClick={() => navigate("/tutorial")}>Tutorial</p>
-        <p className="telaInicial-botao" onClick={() => navigate("/pontuacao")}>Pontuação</p>
-      </div>
-    </div>
+    const voltar = () => {
+      navigate('/'); 
+  };
+  
+        return (
+          <div className="tutorial">
+           <img src={RaioIcone} className="tutorial-icone-raio" alt="Ícone de Raio" />
+            <span className="tutorial-flashcards">FLASHCARDS</span>
+            <div className="tutorial-container">
+              <span className="como">Como jogar:</span>
+              <div className="texto1-tutorial">
+                <li>
+                  O jogo consiste em perguntas e respostas, a cada nível será apresentado um desafio de programação em JAVA,
+                  o seu objetivo é escrever o código correto para passar de fase.
+                </li>
+              </div>
+              <div className="texto2-tutorial">
+                <li>
+                  Lembre-se de que cada vírgula importa, caso algo esteja minimamente fora de ordem, ou faltando um ponto e vírgula,
+                  a sua resposta será considerada errada.
+                </li>
+              </div>
+            </div>
+            <button className="tutorial-botao" onClick={voltar}>
+              <p className="botao-entendi">Entendi!</p>
+            </button>
+            </div>
   );
 }
 
-export default TelaInicial;
+export default Tutorial;
