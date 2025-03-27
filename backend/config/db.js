cd5b3d092173b2134b00d1860509220903600909
Bruninha19 <bruna.tonelli@edu.unifil.br>
2025-03-27 19:50:15 -0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host: "localhost",  
     user: "root",      
-    password: "1104", 
+    password: "1707", 
     database: "flashcards"
 });
 
