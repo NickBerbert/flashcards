5c8e58b1a122579edddf0840dbb2a973603e7170
Bruninha19 <bruna.tonelli@edu.unifil.br>
2025-03-27 14:27:18 -0300

    update

--- src/PerguntaOptativa.js
+++ src/PerguntaOptativa.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./perguntaOptativa.css";
 import RaioIcone from "./public/luxa.org-pixelate-01-raio-png-removebg-preview.png";
-import { useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function PerguntaOptativa() {
   const location = useLocation();
+  const navigate = useNavigate();
   const { questao } = location.state || {}; // Acessando a questão passada pela navegação
   const [respostaUsuario, setRespostaUsuario] = useState("");
   const [mensagem, setMensagem] = useState("");
@@ -39,31 +40,28 @@ function PerguntaOptativa() {
 
   return (
     <>
-      <div className="logo">
-        <img src={RaioIcone} className="perguntaOptativa-icone-raio" alt="Ícone de Raio" />
-        <span className="perguntaOptativa-flashcards">FLASHCARDS</span>
-      </div>
-
-      <div className="perguntaOptativa-pergunta">
-        <span className="pergunta1">
-          {questao ? questao.enunciado : "Carregando questão..."}
-        </span>
+      <div className="perguntaOptativa-tudo">
+      <div className="perguntaOptativa-logo">
+        <img
+          className="perguntaOptativa-icone-raio"
+          src={RaioIcone}
+          alt="imagem-raio"
+        />
+        <div className="perguntaOptativa-logo-text">
+          <div className="perguntaOptativa-flashcards">FLASHCARDS</div>
+        </div>
       </div>
 
       <div className="perguntaOptativa-resposta">
-        <input
-          type="text"
-          placeholder="Digite a resposta"
-          value={respostaUsuario}
-          onChange={(e) => setRespostaUsuario(e.target.value)}
-        />
-        <button className="perguntaOptativa-botaoEnviar" onClick={enviarResposta}>
-          <p className="perguntaOptativa-enviar">Enviar</p>
+        <div className="perguntaOptativa-pergunta">
+          <span className="perguntaOptativa-pergunta1">Pergunta</span>
+        </div>
+
+        <button className="botaoProxima">
+          <p className="proxima">Próxima</p>
         </button>
       </div>
-
-      {mensagem && <p>{mensagem}</p>}
-      {respostaCorreta && <p><strong>Resposta correta:</strong> {respostaCorreta}</p>}
+    </div>
     </>
   );
 }
