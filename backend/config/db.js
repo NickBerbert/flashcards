dfab1550ed59b1e4ee371ff1247ed13bf07e3d41
Nicolas Berbert <nicolasberbert@edu.unifil.com>
2025-05-26 09:49:40 -0300

    senha alterada

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,8 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host: "localhost",  
-    user: "root",      
+    user: "root",     
+    password: "159487236nN+", 
     database: "flashcards"
 });
 
